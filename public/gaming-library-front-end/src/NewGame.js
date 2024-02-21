db6a6c5519ae919a8dd28ba3011a540748aae362
WHDesigns <wyatt.haak4@gmail.com>
2024-02-21 15:39:50 -0800

    added the form elements for the game

--- public/gaming-library-front-end/src/NewGame.js
+++ public/gaming-library-front-end/src/NewGame.js
@@ -1,12 +1,25 @@
 const NewGame = () => {
-    // Title, completion statues, hours played, user score, favorite, notes, release date
-    return ( 
-        <div>
-            <form>
-                
-            </form>
-        </div>
-     );
-}
- 
+  // Title, completion statues, hours played, user score, favorite, notes, release date
+  return (
+    <div>
+      <form>
+        <fieldset>
+          <label>
+            Title
+            <input name="title" placeholder="Minecraft" type="text" />
+          </label>
+          <label>
+            Release Date
+            <input type="date" name="releaseDate" aria-label="Date" />
+          </label>
+          <label>
+            <input type="checkbox" name="favorite" />
+            Favorite
+          </label>
+        </fieldset>
+      </form>
+    </div>
+  );
+};
+
 export default NewGame;
