24b41fc5f14a0eb599d2bf6d595cd5f737531de1
WHDesigns <wyatt.haak4@gmail.com>
2024-02-21 15:33:37 -0800

    added the new game form component

---
+++ public/gaming-library-front-end/src/NewGame.js
@@ -0,0 +1,12 @@
+const NewGame = () => {
+    // Title, completion statues, hours played, user score, favorite, notes, release date
+    return ( 
+        <div>
+            <form>
+                
+            </form>
+        </div>
+     );
+}
+ 
+export default NewGame;
